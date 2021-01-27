3850b633dd5a4bacac942d4cf44bf03e87f26cc9
Jonathan Harris <jon@spacedmonkey.co.uk>
2021-01-27 00:27:43 +0000

    RTL support

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,19 @@ const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
  * External dependencies
  */
 const OptimizeCSSAssetsPlugin = require( 'optimize-css-assets-webpack-plugin' );
+const WebpackRTLPlugin = require( 'webpack-rtl-plugin' );
+const WebpackBar = require( 'webpackbar' );
+const path = require( 'path' );
 
 module.exports = {
 	...defaultConfig,
+	entry: {
+		theme: path.resolve( process.cwd(), 'src', 'theme.js' ),
+	},
+	output: {
+		...defaultConfig.output,
+		path: path.resolve( process.cwd(), 'assets' ),
+	},
 	optimization: {
 		...defaultConfig.optimization,
 		minimizer: [
@@ -28,4 +38,14 @@ module.exports = {
 			},
 		},
 	},
+	plugins: [
+		...defaultConfig.plugins,
+		new WebpackRTLPlugin( {
+			filename: 'theme-rtl.css',
+		} ),
+		new WebpackBar( {
+			name: 'Theme',
+			color: '#fddb33',
+		} ),
+	],
 };
